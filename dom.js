518375fe17f320d862fc00f1b2015071fa9b7397
nelsonkimani63 <nelsonkimani63@gmail.com>
2022-03-23 11:02:03 +0300

    dom manipulation learning

---
+++ dom.js
@@ -0,0 +1,9 @@
+let nameEle = document.getElementById('name1');
+console.log(nameEle);
+
+let saleEle = document.getElementById('sale1');
+console.log(saleEle);
+
+let qtyEle = document.getElementById('qty');
+
+console.log(qtyEle);
